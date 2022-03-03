223e635574b0c1092af25f1aedeed6f373970f7f
ThiagoTa <thiago.teixeirapf@gmail.com>
2022-03-03 11:09:14 -0300

    Propriedades

--- AplicativoFuncional/Frontend/src/App.js
+++ AplicativoFuncional/Frontend/src/App.js
@@ -4,12 +4,28 @@ import Header from './Components/Header';
 
 // <>: fragmento para envolver mais componentes mas não atrapalha o efeito visual do dom
 
+ /**
+  * * Componente <Header />
+  * * Propriedades <Header title="Homepage" />
+  * Estado 
+  */
+
 function App() {
   return (
     <> 
-    <Header />
-    <Header />
-    <Header />
+    <Header title="Homepage">
+      <ul>
+        <li>Homepage</li>
+        <li>Projects</li>
+      </ul>
+    </Header>
+    <Header title="Projects">
+      <ul>
+        <li>Homepage</li>
+        <li>Projects</li>
+        <li>Login</li>
+      </ul>
+    </Header>
     </>
   );
 }
