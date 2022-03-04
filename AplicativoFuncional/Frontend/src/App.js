15f38436cd6a0503c3dc96f374a7b0abc96bdbc7
ThiagoTa <thiago.teixeirapf@gmail.com>
2022-03-04 09:32:58 -0300

    Conectando com o backend

--- AplicativoFuncional/Frontend/src/App.js
+++ AplicativoFuncional/Frontend/src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpg'
 
 import Header from './Components/Header';
 
-// <>: fragmento para envolver mais componentes mas não atrapalha o efeito visual do dom
+// * <>: fragmento para envolver mais componentes mas não atrapalha o efeito visual do dom
 
  /**
   * * Componente <Header />
@@ -13,16 +13,22 @@ import Header from './Components/Header';
   * * Estado e Imutabilidade (Conceito utlizado para garantir performace mesmo em aplicações com muitos dados)
   */
 
-  // useState retorna um array com 2 posíções
+  // * useState retorna um array com 2 posíções
   //
-  // 1. Variável com o seu valor inicial
-  // 2. Função para atualizarmos esse valor
+  // * 1. Variável com o seu valor inicial
+  // * 2. Função para atualizarmos esse valor
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []); // Vai executar essa função somente quando o componente for exibido em tela.
 
   function handleAddProject() {
-    // projects.push(`Novo projeto ${Date.now()}`) * Altera o project 
+    // projects.push(`Novo projeto ${Date.now()}`) * Altera o project e não é recomendado
 
     // ...: está copiando todos os projects para dentro desse novo array
     setProjects([...projects, `Novo projeto ${Date.now()}`]); // Imutabilidade
@@ -32,10 +38,8 @@ function App() {
     <> 
     <Header title="Projects" />
 
-    <img width={300} src={backgroundImage} />
-
     <ul>
-      {projects.map(project => <li key={project}>{project}</li>)}
+      {projects.map(project => <li key={project.id}>{project.title}</li>)}
     </ul>
 
     <button type='button' onClick={handleAddProject}>Adicionar projeto</button>
